76b900b0e2fcf8470cfbec03d327abe2f4d99a64
ghmahta <gh.mahta@yahoo.com>
2024-10-05 13:28:26 +0200

    feat: click on movies or tv shows go to details page implemented

--- src/app/features/media-box/media-box.component.ts
+++ src/app/features/media-box/media-box.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-media-box',
@@ -9,8 +10,17 @@ import {Component, Input} from '@angular/core';
 })
 export class MediaBoxComponent {
   @Input() mediaItem: any;
+  @Input() mode: string = 'movie';
+
+  constructor(private router: Router) {
+  }
 
   getPosterUrl(path: string): string {
     return `https://image.tmdb.org/t/p/w500${path}`;
   }
+
+  routeDetail(){
+    return this.router.navigate([`/details/${this.mode}/${this.mediaItem.id}`]);
+  }
+
 }
