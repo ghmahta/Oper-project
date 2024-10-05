dc9c1efb46ccae403b019e036a40ec608bfeee96
ghmahta <gh.mahta@yahoo.com>
2024-10-05 14:11:05 +0200

    fix: make the active tab bold

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterLink} from '@angular/router';
 import {SearchComponent} from '../search/component/search.component';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -12,6 +13,18 @@ import {SearchComponent} from '../search/component/search.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  currentUrl: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Subscribe to router events to get the current URL
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd) // Only trigger on navigation end
+    ).subscribe((event: NavigationEnd) => {
+      this.currentUrl = event.url;
+      console.log('Current URL:', this.currentUrl); // This will log the current URL in the console
+    });
+  }
 }
